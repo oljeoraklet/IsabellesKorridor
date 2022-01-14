c5832d6bdaaa9b8018cba3555e55658d48df55aa
oljeoraklet <94622064+oljeoraklet@users.noreply.github.com>
2022-01-14 14:06:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+//Date calculations
+const currentDate = new Date();
+const oneJan = new Date(currentDate.getFullYear(),0,1);
+const numberOfDays = Math.floor((currentDate - oneJan) / (24 * 60 * 60 * 1000));
+const currentWeek = Math.ceil(( currentDate.getDay() + 1 + numberOfDays) / 7);
+const testWeek = 43;
+
+//Selecting HTML elements
+const roomBtn = document.querySelector('.dropbtn');
+const dropDown = document.querySelector('.dropdown-menu');
+const roomButtons = document.querySelectorAll('.dropdown-menu button');
+const fullContainer = document.querySelector('.container');
+const headerCleaning = document.getElementById('header-cleaning');
+const cleaningButtons = document.querySelectorAll('.cleaning-info button');
+const exitButtons = document.querySelectorAll('.fa-times-circle');
+const cleaningInfos = document.querySelectorAll('.info-container div');
+const dogVideo = document.querySelector('.cute-dog-video');
+const cleaningInfoContainer = document.querySelector('.cleaning-info ');
+const roomInfo = document.querySelector('.room-info');
+
+
+//Constants with room
+const numberOfRooms = roomButtons.length;
+const rooms = [];
+
+//Adding eventlisteners
+
+//Show the dropdown when the room button is pressed
+roomBtn.onclick = () => {
+    console.log('click');
+    dropDown.classList.toggle('show');
+    roomBtn.classList.toggle('hide')
+}
+
+roomButtons.forEach((roomBtn, index) => {
+    const currentRoom = roomBtn.innerHTML;
+    rooms.push(currentRoom);
+
+    roomBtn.addEventListener('click', (e) => {
+            if (e.target !== e.currentTarget) return;
+            IsItCleaningWeek(index);
+    });
+})
+
+cleaningButtons.forEach((cleaningBtn, index) => {
+    cleaningBtn.addEventListener('click', (e) => {
+        if (e.target !== e.currentTarget) return;
+        ShowRoomInfo(index);
+    })
+})
+
+exitButtons.forEach((exitButton) => {
+    exitButton.addEventListener('click', (e) => {
+        if (e.target !== e.currentTarget) return;
+        HideRoomInfo();
+    })
+})
+
+
+
+//Cleaning week calculation
+const IsItCleaningWeek = (roomNumber) => {
+
+    if (testWeek % numberOfRooms === roomNumber){
+        ChangeThePage("cleaning")
+    }
+    else {
+        ChangeThePage("relax")
+    }
+
+}
+
+const ChangeThePage = (typeOf) => {
+    //Change CSS-settings
+    fullContainer.classList.add(`${typeOf}-week`);
+
+    //Change text
+    headerCleaning.innerHTML = `It's ${typeOf} time!`;
+
+    HideFirstPage();
+
+    if(typeOf === "cleaning") {
+        cleaningInfoContainer.classList.remove('hide')
+    }
+    else {
+        dogVideo.classList.remove('hide')
+    }
+}
+
+const HideFirstPage = () => {
+    roomInfo.classList.add('hide');
+}
+
+const ShowRoomInfo = (selRoom) => {
+   cleaningInfos[selRoom].classList.add('show');
+}
+
+const HideRoomInfo = () => {
+    cleaningInfos.forEach((cleaningInfo) => {
+        cleaningInfo.classList.remove('show');
+    })
+}
+
